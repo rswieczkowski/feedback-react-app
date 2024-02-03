274832f6248cd42caface64dbd29ba6b3ebcf587
Rafał Świeczkowski <rswieczkowski@leonsoftware.com>
2024-02-03 12:46:21 +0100

    Card component and conditional styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
+import Card from './components/shared/Card';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,6 @@
+import PropTypes from 'prop-types';
 import FeedbackItem from './FeedbackItem';
+
 function FeedbackList({ feedback }) {
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>;
@@ -12,5 +14,15 @@ function FeedbackList({ feedback }) {
     </div>
   );
 }
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+};
 
 export default FeedbackList;

0c9b078da57bb973061866a21fff32bc0c67a1d4
Rafał Świeczkowski <rswieczkowski@leonsoftware.com>
2024-02-19 12:44:48 +0100

    Move functions to FeedbackContext

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import { v4 as uuidv4 } from 'uuid';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
@@ -11,19 +9,6 @@ import AboutPage from './pages/AboutPage';
 import { FeedbackProvider } from './context/FeedbackContext';
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData);
-
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
-  };
-
-  const deleteFeedback = (id) => {
-    if (window.confirm('Are you sure you want to delete?')) {
-      setFeedback(feedback.filter((item) => item.id !== id));
-    }
-  };
-
   return (
     <FeedbackProvider>
       <Router>
@@ -35,9 +20,9 @@ function App() {
               path='/'
               element={
                 <>
+                  <FeedbackForm />
                   <FeedbackStats />
-                  <FeedbackForm handleAdd={addFeedback} />
-                  <FeedbackList handleDelete={deleteFeedback} />
+                  <FeedbackList />
                 </>
               }
             ></Route>
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,15 +1,30 @@
 import { createContext, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
-    { id: 1, text: 'This item is from context', rating: 10 },
+    { id: 1, text: 'This is feedback number 1', rating: 10 },
   ]);
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
+        deleteFeedback,
+        addFeedback,
       }}
     >
       {children}

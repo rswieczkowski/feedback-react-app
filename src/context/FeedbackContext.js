65cdff59114d1f9a3b20752c83bc5cddfd5f7578
Rafał Świeczkowski <rswieczkowski@leonsoftware.com>
2024-02-21 11:42:24 +0100

    Add edditing feedback icon

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -6,7 +6,15 @@ const FeedbackContext = createContext();
 export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
     { id: 1, text: 'This is feedback number 1', rating: 10 },
+    { id: 2, text: 'This is feedback number 2', rating: 10 },
+    { id: 3, text: 'This is feedback number 3', rating: 10 },
+    { id: 4, text: 'This is feedback number 4', rating: 10 },
+    { id: 5, text: 'This is feedback number 5', rating: 10 },
   ]);
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
 
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
@@ -19,12 +27,21 @@ export const FeedbackProvider = ({ children }) => {
     setFeedback([newFeedback, ...feedback]);
   };
 
+  // Set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
+  };
+
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
         deleteFeedback,
         addFeedback,
+        editFeedback,
       }}
     >
       {children}

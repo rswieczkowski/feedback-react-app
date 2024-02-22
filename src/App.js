bffc08c6e3b3a27dde37b6327953cf87d21d7788
Rafał Świeczkowski <rswieczkowski@leonsoftware.com>
2024-02-22 13:42:07 +0100

    Remove unused import

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import AboutIconLink from './components/AboutIconLink';
-import FeedbackData from './data/FeedbackData';
 import AboutPage from './pages/AboutPage';
 import { FeedbackProvider } from './context/FeedbackContext';
 
